78b5f0c314ed649bf4d67989d35f09f5920ee2c4
DhamarPutra <damarputra0191@gmail.com>
2024-08-19 05:45:02 +0700

    New Project

--- Bengkel/assets/js/index.js
+++
@@ -1,1 +0,0 @@
-document.getElementById('hamburger-menu-desktop').onclick = function() {    document.getElementById('menu-desktop').classList.toggle('hidden');};document.getElementById('hamburger-menu-mobile').onclick = function() {    document.getElementById('menu-mobile').classList.toggle('hidden');};
