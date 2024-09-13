903a730f37001023eeed9d660d4d66f29c8e69a5
DhamarPutra <damarputra0191@gmail.com>
2024-09-13 23:07:20 +0700

    adding new web

---
+++ Bengkel/assets/js/index.js
@@ -0,0 +1,6 @@
+document.getElementById("hamburger-menu-desktop").onclick = function () {
+  document.getElementById("menu-desktop").classList.toggle("hidden");
+};
+document.getElementById("hamburger-menu-mobile").onclick = function () {
+  document.getElementById("menu-mobile").classList.toggle("hidden");
+};
